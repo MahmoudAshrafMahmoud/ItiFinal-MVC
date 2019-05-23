0ac595ce823c63c134e466b4538ec5156bc084ad
hayam.saleh.as@gmail.com <hayam.saleh.as@gmail.com>
2019-05-23 13:15:45 +0200

    layout1

--- Crafts/Controllers/CartController.cs
+++ Crafts/Controllers/CartController.cs
@@ -1,19 +1,22 @@
-﻿using DAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BL;
+using DAL;
 
 namespace Crafts.Controllers
 {
     public class CartController : Controller
     {
+        List<BL.Order> newd = new List<BL.Order>();
 
-        CraftsEntities Context = new CraftsEntities();
+
         // GET: Cart
         public ActionResult Index()
         {
+           
             return View();
         }
     }
