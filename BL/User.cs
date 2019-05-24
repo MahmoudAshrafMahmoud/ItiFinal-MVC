98f2a254696ea864bc18de0a694a4c5497f44c06
MahmoudAshrafMahmoud <mahmoud271931@science.tanta.edu.eg>
2019-05-24 02:13:30 +0200

    basic Home page with static content

--- BL/User.cs
+++ BL/User.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BL.SharedModels;
+using DAL;
+
 
 namespace BL
 {
     public class User
     {
-    
+        CraftsEntities context = new CraftsEntities();
+
+        public User_table login(string User_Email, string Password)
+        {
+            User_table user = context.User_table.Where(s => s.User_Email.Equals(User_Email) && s.Password.Equals(Password)).FirstOrDefault();
+            return user;
+        }
     }
 }
--- Crafts/Controllers/UserController.cs
+++ Crafts/Controllers/UserController.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BL;
+using BL.SharedModels;
+using DAL;
 
 namespace Crafts.Controllers
 {
     public class UserController : Controller
     {
+        User ul = new User();
         // GET: User
-        public ActionResult Index()
+        public ActionResult Home()
         {
+            if (Session["user"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("login", "User");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult login()
+        {
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult login(string User_Email, string Password)
+        {
+            User_table user = ul.login(User_Email, Password);
+            if (user != null)
+            {
+                Session.Add("user", user);
+                return RedirectToAction("Home", "User");
+
+            }
+            else
+            {
+                ViewBag.NotValidUser = "User Name OR Password is Incorrect";
+                return View("login");
+            }
+
         }
     }
 }
