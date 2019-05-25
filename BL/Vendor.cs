af700cece31e66cd4bb9536dc23949671994cbd2
Omar <Omar@DESKTOP-I8AFTN9>
2019-05-25 03:24:37 +0200

    omar night commit

--- BL/Vendor.cs
+++ BL/Vendor.cs
@@ -124,5 +124,32 @@ namespace BL
 
             return selectVendor;
         }
+
+        public List<OrderModel> SearchOrder(string search,int v_id)
+        {
+            var query = (from v in context.OrderDetails_table
+                         join s in context.Order_table
+                         on v.Order_Id equals s.Order_Id
+                         join l in context.Product_table
+                         on v.Pro_Id equals l.Product_Id
+                         where v.Approval == "pending" && l.Vendor_id == v_id && l.Product_Name.Contains(search)
+                         select new OrderModel()
+                         {
+                             Product_Name = l.Product_Name,
+                             Product_Price = l.Product_Price,
+                             Quantity = v.Quantity,
+                             Order_id = s.Order_Id,
+                             Vendor_id = l.Vendor_id,
+                             Approval = v.Approval,
+                             Order_Date = s.Order_Date.ToString(),
+                             Order_Address = s.Order_Address,
+                             Order_Phone = s.Order_Phone,
+                             Expected_Price = (float)s.Expected_Price,
+                             Image = (byte[])l.Image,
+                             OrderDetail_Id = v.OrderDetail_Id
+                         }).ToList();
+            return query;
+            
+        }
     }
 }
